23ad81eac8c2fba4cdd323651b520d8e75e6b069
Anthea Abreo <anthea.abreo@gmail.com>
2022-10-26 15:57:58 -0500

    correction for getpinfo

--- xv6/kernel/defs.h
+++ xv6/kernel/defs.h
@@ -119,7 +119,7 @@ void            yield(void);
 unsigned        lcg_parkmiller(unsigned *state);
 unsigned        next_random();
 /* getpinfo */
-//int             getpinfo(struct pstat *ps);
+int             getpinfo(void);
 /* End of Code Added */
 
 // swtch.S
