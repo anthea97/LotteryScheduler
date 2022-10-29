3a87bfb5a6c40ec42d8cf03fd7edf387d5bad44f
Anthea Abreo <anthea.abreo@gmail.com>
2022-10-29 11:33:51 -0500

    Add graph.c to read process info

---
+++ xv6/user/graph.c
@@ -0,0 +1,24 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "pstat.h"
+
+#define check(exp, msg) if(exp) {} else {\
+   printf(1, "%s:%d check (" #exp ") failed: %s\n", __FILE__, __LINE__, msg);\
+   exit();}
+
+int
+main(int argc, char *argv[])
+{
+    struct pstat st;
+    int i;  
+
+    check(getpinfo(&st) == 0, "getpinfo");
+    
+    for(i = 0; i < NPROC; i++) {
+        if (st.inuse[i]) {
+            printf(1, "pid: %d tickets: %d ticks: %d\n", st.pid[i], st.tickets[i], st.ticks[i]);
+        }
+    }
+
+}
